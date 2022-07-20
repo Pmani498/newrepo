42c2862c6b09a7b19efb102d6fcca0ab511dcc08
Admin <Admin@DESKTOP-SHFQC7C>
2022-07-20 21:04:18 +0530

    change done

---
+++ sample/src/test/java/test/Trail.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Trail {
+	@Test
+	public void Demo() {
+		System.out.println("First jenkins execution");
+	}
+
+	@Test
+	public void Demotwo() {
+		System.out.println("Second jenkins is executed");
+		System.out.println("hiiii");
+	}
+
+}
